a0540b1c44891913fe3f634c74be6dd9c22cd0c0
ganesh714 <evvganesh1@gmail.com>
2026-03-23 14:28:01 +0530

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/software/bank_account_manager/BankAccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.bank_account_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
