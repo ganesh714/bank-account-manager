7c905884f73c11eb29ba978aae7cff3367615c19
ganesh714 <evvganesh1@gmail.com>
2026-05-06 21:34:10 +0530

    updated docker file

--- src/main/java/com/software/bank_account_manager/config/AxonConfig.java
+++ src/main/java/com/software/bank_account_manager/config/AxonConfig.java
@@ -8,10 +8,31 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AxonConfig {
-	
-	@Bean
-	public SnapshotTriggerDefinition bankAccountSnapshotTriggerDefinition(Snapshotter snapshotter) {
-        // Requirement #11: Create a snapshot after every 5 events
+
+    // Requirement #11: Create a snapshot after every 5 events
+    @Bean
+    public SnapshotTriggerDefinition bankAccountSnapshotTriggerDefinition(Snapshotter snapshotter) {
         return new EventCountSnapshotTriggerDefinition(snapshotter, 5);
+    }
+
+    @Bean
+    public org.axonframework.eventsourcing.eventstore.EventStorageEngine storageEngine(
+            org.axonframework.common.jpa.EntityManagerProvider entityManagerProvider,
+            org.axonframework.common.transaction.TransactionManager transactionManager) {
+        return org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine.builder()
+                .entityManagerProvider(entityManagerProvider)
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+    @Bean
+    public org.axonframework.eventsourcing.eventstore.EmbeddedEventStore eventStore(
+            org.axonframework.eventsourcing.eventstore.EventStorageEngine storageEngine,
+            org.axonframework.spring.config.AxonConfiguration configuration) {
+        return org.axonframework.eventsourcing.eventstore.EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(configuration
+                        .messageMonitor(org.axonframework.eventsourcing.eventstore.EventStore.class, "eventStore"))
+                .build();
     }
 }
