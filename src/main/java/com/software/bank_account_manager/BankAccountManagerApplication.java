edec767d91f945351ff834c9ba001f30c1b436da
ganesh714 <evvganesh1@gmail.com>
2026-05-06 22:09:51 +0530

    fix: Add @EntityScan to scan Axon JPA entities

--- src/main/java/com/software/bank_account_manager/BankAccountManagerApplication.java
+++ src/main/java/com/software/bank_account_manager/BankAccountManagerApplication.java
@@ -3,7 +3,10 @@ package com.software.bank_account_manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 @SpringBootApplication
+@EntityScan(basePackages = {"org.axonframework.eventsourcing.eventstore.jpa", "org.axonframework.eventhandling.tokenstore.jpa", "org.axonframework.modelling.saga.repository.jpa", "com.software.bank_account_manager"})
 public class BankAccountManagerApplication {
 
 	public static void main(String[] args) {
