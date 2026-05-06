43387fabe1155bf45412fee8fae81bf39e90df67
ganesh714 <evvganesh1@gmail.com>
2026-05-06 22:17:55 +0530

    fix: Update EntityScan import for Spring Boot 4.0.4

--- src/main/java/com/software/bank_account_manager/BankAccountManagerApplication.java
+++ src/main/java/com/software/bank_account_manager/BankAccountManagerApplication.java
@@ -3,7 +3,7 @@ package com.software.bank_account_manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"org.axonframework.eventsourcing.eventstore.jpa", "org.axonframework.eventhandling.tokenstore.jpa", "org.axonframework.modelling.saga.repository.jpa", "com.software.bank_account_manager"})
